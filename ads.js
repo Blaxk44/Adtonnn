9b64f900b8f07c589cde7eef08583a8a87652add
Blaxk44 <167696379+Blaxk44@users.noreply.github.com>
2026-02-07 21:31:11 +0530

    Add files via upload

---
+++ ads.js
@@ -0,0 +1,17 @@
+import { updateDoc, increment } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { calculateReward } from "./rewards.js";
+
+export async function showMonetagAd(userRef, adsWatched) {
+  show_10551237().then(async () => {
+    const reward = calculateReward(adsWatched);
+    await updateDoc(userRef, {
+      balance: increment(reward),
+      ads_watched: increment(1),
+      today_earnings: increment(reward)
+    });
+
+    Telegram.WebApp.HapticFeedback.notificationOccurred("success");
+    document.getElementById("rewardSound").play();
+    alert(`+${reward} TON earned`);
+  });
+}
